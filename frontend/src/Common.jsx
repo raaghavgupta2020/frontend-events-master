ad4469016348ec9c2fe9d97c29743adb7306b6a9
raaghavgupta2020 <raaghavgupta2020@gmail.com>
2022-08-18 10:59:19 +0530

    head

--- frontend/src/Common.jsx
+++ frontend/src/Common.jsx
@@ -9,19 +9,19 @@ function Common() {
                 justifyContent: 'center',
                 alignItems: 'center',
             }}>
-                <h1 className='text-5xl pt-4 font-bold tracking-wider absolute top-12'>Welcome   to   Warrantify !</h1>
+                <h1 className='text-5xl pt-4 font-bold tracking-wider absolute top-12'>Eventify!</h1>
             </div>
             <div className="flex items-center justify-center h-screen">
 
                 <button className="relative bg-blue-500 w-60 h-60 text-2xl hover:bg-blue-700 text-white mx-5 font-bold px-4 rounded">
 
                     <Link to="/loginuser">
-                        Buyer
+                        Participant
                     </Link>
                 </button>
                 <Link to="/logincreator">
                     <button className="relative bg-yellow-300 h-60	w-60  text-2xl  hover:bg-yellow-400 text-white mx-5 font-bold px-4 rounded">
-                        Seller
+                        Admin
                     </button>
                 </Link>
             </div>
